0390b9a1fc7f8ebada71948512f228a6cb10898a
Sérgio Giraldo <sergiorgiraldo@gmail.com>
2015-09-25 15:59:59 -0300

    second version

--- src/KMeansMP.java
+++ src/KMeansMP.java
@@ -17,6 +17,42 @@ import scala.Tuple2;
 
 public final class KMeansMP {
 	// TODO
+    private static class ParsePoint implements Function<String, Vector> {
+        private static final Pattern SPACE = Pattern.compile(",");
+
+        public Vector call(String line) {
+            String[] tok = SPACE.split(line);
+            double[] point = new double[tok.length-1];
+            for (int i = 1; i < tok.length; ++i) {
+                point[i-1] = Double.parseDouble(tok[i]);
+            }
+            return Vectors.dense(point);
+        }
+    }
+
+    private static class ParseTitle implements Function<String, String> {
+        private static final Pattern SPACE = Pattern.compile(",");
+
+        public String call(String line) {
+            String[] tok = SPACE.split(line);
+            return tok[0];
+        }
+    }
+
+    private static class ClusterCars implements PairFunction<Tuple2<String, Vector>, Integer, String> {
+        private KMeansModel model;
+        public ClusterCars(KMeansModel model) {
+            this.model = model;
+        }
+
+        public Tuple2<Integer, String> call(Tuple2<String, Vector> args) {
+            String title = args._1();
+            Vector point = args._2();
+            int cluster = model.predict(point);
+            return new Tuple2<Integer, String>(cluster, title);
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println(
@@ -30,14 +66,19 @@ public final class KMeansMP {
         int iterations = 100;
         int runs = 1;
         long seed = 0;
-		final KMeansModel model;
+	final KMeansModel model;
 		
         SparkConf sparkConf = new SparkConf().setAppName("KMeans MP");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-        //TODO
+        JavaRDD<String> lines = sc.textFile(inputFile);
+        JavaRDD<Vector> parsedPoints = lines.map(new ParsePoint());
+        JavaRDD<String> parsedTitles = lines.map(new ParseTitle());
 
-        //results.saveAsTextFile(results_path);
+	model = KMeans.train(parsedPoints.rdd(), k, iterations, runs, KMeans.RANDOM(), seed);
+
+        results = parsedTitles.zip(parsedPoints).mapToPair(new ClusterCars(model)).groupByKey();
+        results.saveAsTextFile(results_path);
 
         sc.stop();
     }
